22e9d334ae06dc83fd386044aa49141b04cdd984
scinamusic <succer115@tiscali.it>
2017-02-04 23:30:12 +0100

    beta release
    Drums ok.
    some equalization
    small code cleanings

--- still_echo/still_echo.rb
+++ still_echo/still_echo.rb
@@ -49,7 +49,6 @@ bass_a_line= [:c2, :g1, :bb1, :c2, :c2,     :r,    :c2, :c2, :bb1, :c2,  :ab1,  
 bass_a_time = [q,   s,   s,    s,  s,        q,      s,  s,    s,   s,     q,    s,     s,   s,  s,        q,  q ]
 
 
-#ok!
 bass_b_line= [:c2, :g1, :bb1, :c2, :c2,  :r, :r, :c2, :g1, :bb1, :c2, :c2,  :r, :r]
 bass_b_time = [q,   s,   s,    s,  s,    q,  q,   q,   s,   s,    s,  s,    q,  q]
 
@@ -57,10 +56,9 @@ bass_c_line = [:c2,  :c2, :c2, :c2,  :c2,  :r,  :c2,   :c2,  :g1,  :bb1, :c2, :r
 bass_c_time = [q,      s,   s,   s,   s,    o,   qp,    q,     s,   s,    s,  s,   qp]
 
 
-
+# harmony
 chords_a = ring   (chord :c, :minor7),   (chord :c, :minor7), (chord :c, :minor7),   (chord :c, :minor7),
   (chord :ab, :major7), (chord :ab, :major7), (chord :ab, :major7), (chord :ab, :major7)
-
 
 chords_b = ring  (chord :ab, :major7), (chord :ab, :major7), (chord :ab, :major7), (chord :ab, :major7),
   (chord :c, :minor),   (chord :c, :minor), (chord :c, :minor),   (chord :c, :minor)
@@ -69,7 +67,11 @@ chords_c = ring   (chord :c, :minor7),   (chord :c, :minor7), (chord :c, :minor7
   (chord :ab, :major7), (chord :ab, :major7), (chord :ab, :major7), (chord :ab, :major7)
 
 
-#play_chord [:e2,:e3,:b3],sustain: (b * 0.9),release: (b * 0.1),amp: 0.4
+
+# drums
+bass_drums_ac = (ring true,false, false,false,  true,false, false, false,   true,false,false,false,   true,false, true,true)
+bass_drums_b = (ring true,false, false,true,  false,false, false, true,   true,false,false,false,   true,false, true,true)
+
 
 
 define :harmony do |notearray,shift=0,vol=1|
@@ -93,12 +95,12 @@ define :trumpet do |notearray,durationarray,shift=0,vol=1|
             with_fx :gverb, damp: 1 do
               in_thread do
                 with_synth :dsaw do
-                  playarray(notearray, durationarray,0,vol=0.8,sust=0.3)
+                  playarray(notearray, durationarray,0,vol=0.04,sust=0.3)
                 end
               end
               in_thread do
                 with_synth :dpulse do
-                  playarray(notearray, durationarray,0,vol=0.8,sust=0.3)
+                  playarray(notearray, durationarray,0,vol=0.04,sust=0.3)
                 end
               end
             end
@@ -115,7 +117,7 @@ define :bass do |notearray,durationarray,shift=0,vol=1|
   in_thread do
     with_fx :band_eq, freq: 50, res: 0.5, db: 3 do
       with_fx :krush, mix: 0.2, amp: 0.5 do
-        with_fx :compressor, pre_amp: 3, threshold: 80  do
+        with_fx :compressor, pre_amp: 1, threshold: 80  do
           with_fx :distortion, distort: 0.8 do
             in_thread do
               with_synth :dsaw do
@@ -139,6 +141,58 @@ define :bass do |notearray,durationarray,shift=0,vol=1|
 end
 
 
+define :drums_ac do
+  in_thread do
+    tick
+    tick
+    16.times do
+      sample solenoid, "hit_5", amp: 0.85, hpf: 100 if (spread, 18, 18).tick
+      with_fx :reverb, amp: 1, room: 1 do
+        sample my_sample, "Korg-M3R-Side-Stick", amp: 2 if (spread, 4, 16).look
+      end
+      sleep 0.5
+    end
+  end
+  
+  in_thread do
+    16.times do
+      tick
+      with_fx :compressor do
+        sample :drum_heavy_kick, amp: 2 if bass_drums_ac.look
+      end
+      sleep 0.5
+    end
+  end
+  
+end
+
+
+define :drums_b do
+  in_thread do
+    tick
+    tick
+    16.times do
+      with_fx :compressor do
+        sample solenoid, "hit_5", amp: 0.85, hpf: 100 if (spread, 18, 18).tick
+      end
+      with_fx :reverb, amp: 1, room: 1 do
+        sample my_sample, "Korg-M3R-Side-Stick", amp: 2 if (spread, 5, 16).look
+      end
+      sleep 0.5
+    end
+  end
+  
+  in_thread do
+    16.times do
+      with_fx :compressor do
+        sample :drum_heavy_kick, amp: 2 if bass_drums_b.tick
+      end
+      sleep 0.5
+    end
+  end
+  
+end
+
 
 # STRUTTURA
 define :intro do
@@ -150,8 +204,6 @@ define :intro do
 end
 
 
-
-
 define :part_a do
   puts "Part A"
   2.times do
@@ -161,17 +213,10 @@ define :part_a do
     #in_thread do
     bass(bass_a_line,bass_a_time)
     harmony(chords_a)
-    drums_ab
+    drums_ac
     sleep 8
   end
-  
-  #in_thread do
-  #
-  #end
-  #sleep 4*2  #end
 end
-
-
 
 define :part_b do
   puts "Part B"
@@ -179,11 +224,44 @@ define :part_b do
     trumpet(trumpet_b_line,trumpet_b_time)
     bass(bass_b_line,bass_b_time)
     harmony(chords_b)
-    drums_ab
+    drums_b
     sleep 8
   end
 end
 
+define :part_c do
+  puts "Part C"
+  2.times do
+    trumpet(trumpet_c_line,trumpet_c_time)
+    bass(bass_c_line,bass_c_time)
+    harmony(chords_c)
+    drums_ac
+    sleep 8
+  end
+end
+
+define :brake_one do
+  puts "Break one"
+  4.times do
+    bass(bass_b_line,bass_b_time)
+    harmony(chords_b)
+    drums_b
+    sleep 4*2
+  end
+end
+
+define :solo do
+  puts "Start solo!"
+  counter=0
+  8.times do
+    bass(bass_a_line,bass_a_time)
+    harmony(chords_a)
+    drums_b
+    sleep 4*2
+    puts "round: ", counter
+    counter=counter-1
+  end
+end
 
 define :part_ending do
   puts "Bye bye"
@@ -195,7 +273,7 @@ define :part_ending do
         trumpet(trumpet_b_line,trumpet_b_time)
         bass(bass_b_line,bass_b_time)
         harmony(chords_b)
-        drums_ab
+        drums_b
         sleep 3*8
       end
     end
@@ -204,84 +282,6 @@ define :part_ending do
   end
 end
 
-
-
-
-
-define :part_c do
-  puts "Part C"
-  2.times do
-    trumpet(trumpet_c_line,trumpet_c_time)
-    bass(bass_c_line,bass_c_time)
-    harmony(chords_c)
-    drums_c
-    sleep 8
-  end
-end
-
-
-define :brake_one do
-  4.times do
-    bass(bass_b_line,bass_b_time)
-    harmony(chords_b)
-    drums_ab
-    sleep 4*2
-  end
-end
-
-
-
-define :solo do
-  8.times do
-    bass(bass_a_line,bass_a_time)
-    harmony(chords_a)
-    drums_ab
-    sleep 4*2
-    puts "round"
-  end
-end
-
-
-
-define :drums_ab do
-  
-  in_thread do
-    tick
-    tick
-    16.times do
-      sample solenoid, "hit_5", amp: 1.8, hpf: 100 if (spread, 18, 18).look
-      sample my_sample, "Korg-M3R-Side-Stick", amp: 4 if (spread, 3, 16).look
-      tick
-      sleep 0.5
-    end
-  end
-  in_thread do
-    16.times do
-      sample :drum_heavy_kick, amp: 1 if (spread 2, 16).tick
-      sleep 0.5
-    end
-  end
-  
-end
-
-
-define :drums_c do
-  
-  in_thread do
-    16.times do
-      sample solenoid, "hit_5", amp: 1, hpf: 85 if (spread, 10, 18).look
-      sample
-      tick
-      sleep 0.5
-    end
-  end
-  in_thread do
-    16.times do
-      sample :drum_heavy_kick, amp: 1 if (spread 2, 16).tick
-    end
-  end
-  
-end
 
 
 
@@ -307,9 +307,9 @@ end
 #set_sched_ahead_time! 0.5 #adjust as necessary.
 with_fx :reverb,room: 0.5 do
   
-  #with_fx :echo, decay: 3, pre_amp: 0.8, max_phase: 0.4, phase: 0.25, max_phase: 0.55 do
-  #  intro #0:00
-  #end
+  with_fx :echo, decay: 3, pre_amp: 0.8, max_phase: 0.4, phase: 0.25, max_phase: 0.55 do
+    intro #0:00
+  end
   
   part_b #0:20 (la seconda volta con due voci)
   part_b
@@ -339,4 +339,6 @@ with_fx :reverb,room: 0.5 do
   part_ending # 5:19  x4 fading...
   
 end
+
+
 
